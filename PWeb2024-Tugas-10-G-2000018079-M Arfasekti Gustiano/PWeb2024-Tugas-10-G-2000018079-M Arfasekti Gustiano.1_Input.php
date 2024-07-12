7a5d62702322781fcade923b84b6841075cc5c97
M. Arfasekti Gustiano A <raanky.ryansyah@gmail.com>
2024-07-12 23:05:46 +0700

    Pweb10

---
+++ PWeb2024-Tugas-10-G-2000018079-M Arfasekti Gustiano/PWeb2024-Tugas-10-G-2000018079-M Arfasekti Gustiano.1_Input.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="style (3).css">
+        <title>Input</title>
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <header>
+			<div class="container">
+				<h1><a href="">Konversi Nilai Huruf Sesuai Dengan Nilai PAP</a></h1>
+
+			</div>
+		</header>
+    <div class="background">   
+      <table style="padding: 25px 50px 75px 100px;">
+        <tr><h2 style="padding-left: 100px; padding-top: 25px;">Masukkan Nilai Yang Ingin DiCek</h2></tr>   
+        <tr>
+        <!-- Penginputan Bilangan yang akan Dicek -->
+        <form action="PWeb2021-ReniMamila-Tugas10.1 Output.php" method="GET">
+        <td>
+            <div class="input-field">
+                <i class="fas fa-user"></i>
+                <input type="number" name="myNumber" id="myNumber" placeholder="Masukkan Angka" />
+              </div>
+              </td>
+              <td style="padding-left: 30px;">
+                    <input  type="submit" id="btCek" name="btCek" value="CEK" class="btn solid" />
+                </td>
+</form>
+    </tr>
+</table> 
+</div> 
+<footer>
+    <div class="container">
+        <small>Dibuat &copy; 2021 - ReniMamila.</small>
+    </div>
+</footer> 
+
+
+    </body>
+</html>
